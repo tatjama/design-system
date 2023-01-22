9af4d2e5e5eb8b3214bd8eb87465bb2d4dd83448
Tanja <tanja120a@gmail.com>
2023-01-22 23:49:39 +0100

    Compiling SCSS to CSS
    * install node-sass
    * create file build.js
    * modify package.json, add into the "scripts" "build" option
    * create lib folder
    * build css with yarn build

---
+++ scss/src/scripts/build.js
@@ -0,0 +1,17 @@
+const Fs = require('fs');
+const Path = require('path');
+const Sass = require('node-sass');
+
+const result = Sass.renderSync({
+    data: Fs.readFileSync(Path.resolve('src/global.scss')).toString(),
+    outputStyle: 'expanded',
+    outFile: 'global.css',
+    includePaths: [Path.resolve('src')]
+})
+
+console.log(result.css.toString());
+
+Fs.writeFileSync(
+    Path.resolve('src/lib/global.css'),
+    result.css.toString()
+)
